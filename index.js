60cd2f5e2d61d65639ab7dda88a77af12d196b85
+ <fadosa.abdirhman@student.moringaschool>
2024-07-05 12:14:47 +0300

    modify

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 10;
+let num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
+
+// Write your code here
